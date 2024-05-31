b62a0879558f273ebdcc865182e1c24bc6c4d8d8
wn <csignore@whril.com>
2024-05-31 14:58:58 -0400

    d

---
+++ app/routes/index.tsx
@@ -0,0 +1,15 @@
+import logo from "~/../public/img/ja.png";
+
+export default function Index() {
+  return (
+    <div className="container">
+      <div className="inner-content">
+      <a className="none" href="https://app.jeffamzn.com/subscription/form"><img src={logo} /></a>
+      <a href="https://app.jeffamzn.com/subscription/form">Subscribe</a>
+      <div className="terms">
+          <p>This site is protected by hCAPTCHA and the Google <a target="_blank" href="https://policies.google.com/privacy?hl=en-US">Privacy Policy</a> and <a target="_blank"  href="https://policies.google.com/terms?hl=en-US">Terms of Service</a> apply.</p>
+      </div>
+      </div>
+    </div>
+  );
+}
