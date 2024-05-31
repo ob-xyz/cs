32d4381ac985608053fb684ccea1cf9ab1679d0f
wn <csignore@whril.com>
2024-05-31 15:20:00 -0400

    s

--- app/root.tsx
+++ app/root.tsx
@@ -27,8 +27,8 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Daily Business Newsletter on Tech at Jeffamzn",
-  description: "Daily business newsletter that delivers prime insights about tech, stocks, and entrepreneurship."
+  title: "Chris Signore",
+  description: "The only blog read by Elon Musk. So you should read it too."
 });
 
 export default function App() {
