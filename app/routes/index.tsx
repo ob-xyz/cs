b6c68553265f727f67c9256a596baa0a0b6a7b69
wn <csignore@whril.com>
2025-03-23 15:30:39 -0400

    182

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -25,7 +25,7 @@ export default function Index() {
       <div className="text">
       <p>Viva La Canada.</p>
       <p>hey! I'm <u>Chris Signore</u> maker of Jeffamazon, the PC Party of Canada, and a new newsletter called The Citizen that'll help me and you actually understand the business of politics.</p>
-      <p>Join me, and 12,000 high agency citizens trying to better understand politics.</p>
+      <p>Join me, and 12,000 progressive, high agency citizens trying to better understand politics.</p>
       </div>
       <form method="post" action="https://app.jeffamzn.com/subscription/form">
           <p><input className="email" type="email" name="email" required placeholder="Email" /></p>
