99a530a24b5532bfa11eae4aa07d0fc75fcaa364
wn <csignore@whril.com>
2025-04-28 21:19:43 -0400

    212

--- public/build/root-7AKUEFA3.js
+++ public/build/root-7AKUEFA3.js
@@ -12,7 +12,7 @@ import {
 } from "/build/_shared/chunk-SBNNL67F.js";
 
 // app/style/global/global.css
-var global_default = "/build/_assets/global-CXPOMUC5.css";
+var global_default = "/build/_assets/global-6DD4QMAD.css";
 
 // app/root.tsx
 var import_jsx_dev_runtime = __toESM(require_jsx_dev_runtime());
@@ -90,4 +90,4 @@ export {
   links,
   meta
 };
-//# sourceMappingURL=/build/root-FGNCOMY2.js.map
+//# sourceMappingURL=/build/root-7AKUEFA3.js.map
